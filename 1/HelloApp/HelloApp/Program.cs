29109b29e0ae48ece236f0a5da37c25328e00c18
sergdark <sergdarkjob@gmail.com>
2019-04-26 13:25:43 +0300

    second commit

--- 1/HelloApp/HelloApp/Program.cs
+++ 1/HelloApp/HelloApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using System;
+using System.Linq;
 
 namespace HelloApp
 {
@@ -6,7 +8,27 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
